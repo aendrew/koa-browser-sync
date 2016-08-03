fedc28a9b7c7ad002f32eb37dc21a0283f82b555
Ændrew Rininsland <andrew.rininsland@ft.com>
2016-08-03 11:55:47 +0100

    First pass at updating koa-browser-sync to Koa 2

--- index.js
+++ index.js
@@ -1,51 +1,55 @@
+/**
+ * koa2-browser-sync
+ * 2016 Ændrew Rininsland, Financial Times
+ *
+ * Based on koa-browser-sync by SimonDegraeve.
+ */
 
-var thunkify       = require('thunkify');
-var browserSync    = thunkify(require('browser-sync').init);
+var browserSync    = require('browser-sync');
 var StreamInjecter = require('stream-injecter');
 
-module.exports = function(opts) {
-
-  var bs, snippet;
+module.exports = function(opts, bs) {
+  var snippet;
   var opts       = opts || {};
   opts.init      = opts.init || false;
   opts.debugInfo = opts.debugInfo || false;
 
-  return function*(next) {
-    yield next;
+  return function(ctx, next) {
+    return next.then(function(){
+      if (opts.init) {
+        if (!bs) bs = browserSync.create().init(opts);
+        snippet = bs.getOption('snippet');
+      } else {
+        snippet = process.env.BROWSERSYNC_SNIPPET;
+      }
 
-    if (opts.init) {
-      if (!bs) bs = yield browserSync(null, opts);
-      snippet = bs.getOption('snippet');
-    } else {
-      snippet = process.env.BROWSERSYNC_SNIPPET;
-    }
+      if (!snippet) return;
 
-    if (!snippet) return;
+      if (!(ctx.response.type && ~ctx.response.type.indexOf("text/html"))) return;
 
-    if (!(this.response.type && ~this.response.type.indexOf("text/html"))) return;
+      // Buffer
+      if (Buffer.isBuffer(ctx.body)) {
+        ctx.body = ctx.body.toString();
+      }
 
-    // Buffer
-    if (Buffer.isBuffer(this.body)) {
-      this.body = this.body.toString();
-    }
+      // String
+      if (typeof ctx.body === 'string') {
+        if (ctx.body.match(/client\/browser-sync-client/)) return;
+        ctx.body = ctx.body.replace(/<\/body>/, snippet + '</body>');
+      }
 
-    // String
-    if (typeof this.body === 'string') {
-      if (this.body.match(/client\/browser-sync-client/)) return;
-      this.body = this.body.replace(/<\/body>/, snippet + '</body>');
-    }
-
-    // Stream
-    if (this.body && typeof this.body.pipe === 'function') {
-      var injecter = new StreamInjecter({
-        matchRegExp: /(<\/body>)/,
-        inject:      snippet,
-        replace:     snippet + '$1',
-        ignore:      /client\/browser-sync-client/
-      });
-      var size = +this.response.header['content-length'];
-      if (size) this.set('Content-Length', size + snippet.length);
-      this.body = this.body.pipe(injecter);
-    }
+      // Stream
+      if (ctx.body && typeof ctx.body.pipe === 'function') {
+        var injecter = new StreamInjecter({
+          matchRegExp: /(<\/body>)/,
+          inject:      snippet,
+          replace:     snippet + '$1',
+          ignore:      /client\/browser-sync-client/
+        });
+        var size = +ctx.response.header['content-length'];
+        if (size) ctx.set('Content-Length', size + snippet.length);
+        ctx.body = ctx.body.pipe(injecter);
+      }
+    });
   };
 };
